79c22ae5f32148fcab96227bfc81d9aed6d6384e
adrianoasbarbosa <adrianodevsistemas@gmail.com>
2024-08-27 20:04:08 -0300

    Atualização NavBar

--- react-website/src/components/Navbar/index.jsx
+++ react-website/src/components/Navbar/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FiLogOut } from 'react-icons/fi'
 import { HeaderContainer, ItensContainer, Logo } from './styles'
 
 export default function Navbar() {
@@ -12,6 +13,9 @@ export default function Navbar() {
                 <ItensContainer href="/">Carrinho</ItensContainer>
                 <ItensContainer href="/">Contatos</ItensContainer>
                 <ItensContainer href="/">Perfil</ItensContainer>
+                <ItensContainer href="/logout">
+                    <FiLogOut size={24} />
+                </ItensContainer>
             </nav>
         </HeaderContainer>
     )
--- react-website/src/components/Navbar/styles.js
+++ react-website/src/components/Navbar/styles.js
@@ -1,31 +1,47 @@
 import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
-position: fixed;
-top: 0;
-left: 0;
-width: 100%;
-padding: 20px 10%;
-background: saddlebrown;
-display: flex;
-justify-content: space-between;
-align-items: center;
-z-index: 100;
- a{
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 20px 10%;
+  background: #000;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 100;
+
+  a {
     text-decoration: none;
- }
-`
+  }
+`;
 
 export const Logo = styled.a`
-font-size: 32px;
-color: #fff;
-font-weight: 700;
-`
+  font-size: 32px;
+  color: #fff;
+  font-weight: 700;
+`;
 
 export const ItensContainer = styled.a`
-position: relative;
-font-size: 18px;
-color: #ff1;
-font-weight: 500;
-margin-left: 40px;
-`
+  position: relative;
+  font-size: 18px;
+  color: #fff;
+  font-weight: 500;
+  margin-left: 40px;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 100%;
+    left: 0;
+    width: 0;
+    height: 2px;
+    background-color: #fff;
+    transition: width 0.3s ease;
+  }
+
+  &:hover::before {
+    width: 100%;
+  }
+`;
